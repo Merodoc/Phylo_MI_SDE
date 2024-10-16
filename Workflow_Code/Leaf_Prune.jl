e2afe8dec96f4a3ed19f6d983934fd6f3b56e6a6
Merodoc <34044733+Merodoc@users.noreply.github.com>
2024-10-16 12:17:48 +1000

    Workflow should be functional now

--- Workflow_Code/Leaf_Prune.jl
+++ Workflow_Code/Leaf_Prune.jl
@@ -1,7 +1,7 @@
 using Phylo
 
 
-mars_tree = open(parse(RootedTree), Phylo.path("C:/Users/Rowan/OneDrive/Documents/GitHub/REG_PhD/Workflow_Code/newtree.nwk"))
+mars_tree = open(parse(RootedTree), Phylo.path("C:/PhD/Phylo_MI_SDE/Workflow_Code/newtree.nwk"))
 #"C:/PhD/Phylo_MI_SDE/Workflow_Code/Data/Mars_TimeTree.nwk"
 #C:/Users/Rowan/OneDrive/Documents/GitHub/REG_PhD/Workflow_Code/Mars_TimeTree.nwk
 
@@ -11,8 +11,8 @@ using DataFrames
 
 # Read in Data
 # Will need to do similar data matching to that in R
-mars = CSV.read("C:/Users/Rowan/OneDrive/Documents/GitHub/REG_PhD/Workflow_Code/mars.csv", DataFrame)
-mars_avg = CSV.read("C:/Users/Rowan/OneDrive/Documents/GitHub/REG_PhD/Workflow_Code/Imp_Mars_PVR25new5.csv", DataFrame, types = [String, Float64])
+mars = CSV.read("C:/PhD/Phylo_MI_SDE/Workflow_Code/mars.csv", DataFrame)
+mars_avg = CSV.read("C:/PhD/Phylo_MI_SDE/Workflow_Code/Imp_Mars_PVR25new5.csv", DataFrame, types = [String, Float64])
 using Statistics
 using Bridge
 #C:/PhD/Phylo_MI_SDE/Workflow_Code
@@ -49,8 +49,15 @@ function Leaf_Prune(tree, start_vals)
 leaves = getleafnames(tree)
 root = first(nodenamefilter(isroot, tree))
 data = DataFrame(Nodes = leaves, Vals = start_vals)
+v = []
+children = ["NA", "NA"]
+push!(v, children)
+data[!, :Children] .= v
 data[!,:Child1] .= 0.
+data[!,:Child1Val] .= 0.
 data[!, :Child2] .= 0.
+data[!,:Child2Val] .= 0.
+data[!,:BridgeVar] .= 0.
 iter = 0
 idx = 0
 vals = start_vals
@@ -74,31 +81,40 @@ for leaf in leaves
     parent = getparent(tree, leaf)
     if parent ∈ leaves 
         continue
-        println("PARENT IN LEAVES")
+        #println("PARENT IN LEAVES")
     end
     #print(iter)
     iter = iter +1
     #might be good time for a try/catch
         #test that all the children of the parent leaf are leaves
     ch = getchildren(tree, parent)
+    children = [getnodename(tree, ch[1]), getnodename(tree,ch[2])]
     if getnodename(tree, ch[1]) ∈ leaves && getnodename(tree, ch[2]) ∈ leaves
-    # not all children are leaves, so return to testing leaves until we find one that does
-    #if all children of parent leaf are leaves then do stuff
-            bridgelen = Vector{Float64}()
+
+        #Pull existing trait values from the data frame
+        row1 = filter(row -> row.Nodes == getnodename(tree, ch[1]), data)
+        row2 = filter(row -> row.Nodes == getnodename(tree, ch[2]), data)
+        val1 = row1.Vals[1]
+        val2 = row2.Vals[1]
+        bridgelen = Vector{Float64}()
     #print(getnodename(mars_tree, parent))
     #optimizing this depends on what inputs the bridge needs
+
             for branch in getoutbounds(tree, parent)
                 len = getlength(tree, branch)
                 push!(bridgelen, len)
             end
+
             time = sum(bridgelen)
-        #have to figure out a better way to assign values to child nodes, maybe make a val, node dict in initialization
-        
-        #bridgesim = Phylo_Bridge()
+            time = round(time, digits = 2)
+            anc_time = round(bridgelen[1], digits = 2)
+            println(anc_time)
+            bridgesim = Phylo_Bridge(val1, val2, time, anc_time, 0.01, 100)
+            Xhat = mean(bridgesim)
+            Var = var(bridgesim)
+
         #bridgelen works - we can now do the diffusion bridge on them
-    
-        
-    push!(data, (parent,0, bridgelen[1], bridgelen[2]))
+    push!(data, (parent,Xhat, children, bridgelen[1], val1, bridgelen[2], val2, Var))
     push!(leaves, parent)
     #Should be able to use bridgelen for bridge operations we wanna try
 
@@ -161,3 +177,32 @@ df[!, :Child1] .= missing
 pruned
 
 row = filter(row -> row.Nodes == "'98'", pruned)
+
+using Plots
+
+Val_Dict = Dict()
+
+for i in 1:length(pruned.Nodes)
+    push!(Val_Dict, pruned.Nodes[i] => pruned.Vals[i])
+end
+
+Val_Dict
+
+plot(mars_tree, showtips = false, marker_z = Val_Dict)
+plot(mars_tree, size = (400,800), linecolor = :orange, linewidth = 5, marker_size = 10, markercolor = :steelblue, markerstrokecolor = :white,
+series_annotations = text.(round.(pruned.Vals, digits = 2),10,:center,:center,:black), tipfont = (4,))
+
+Val_Dict.keys
+
+CSV.write("BrownianBridge_Test2.csv", pruned)
+
+v = []
+children = ["78", "38"]
+push!(v, children)
+df[!, :Children] .= v
+
+newData = pruned[!, 1:3]
+newData[!,:Var] = pruned.BridgeVar
+
+
+CSV.write("BrownianBridge_Test3.csv", newData)
