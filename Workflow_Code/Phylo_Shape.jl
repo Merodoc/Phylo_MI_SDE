06167458f1849778dc18f8e70dfc9ed378d68693
Merodoc <rowan@overworld.net.au>
2025-02-06 14:18:06 +1000

    data_loaded

--- Workflow_Code/Phylo_Shape.jl
+++ Workflow_Code/Phylo_Shape.jl
@@ -46,7 +46,7 @@ end
 
 
 #dir = "C:/PhD/Phylo_MI_SDE/Workflow_Code/Sampled2l_241024/"
-dir = "C:/Users/Rowan/OneDrive/Documents/GitHub/REG_PhD/Workflow_Code/Sampled2l_241024/"
+dir = "C:/Users/Rowan/OneDrive/Documents/GitHub/REG_PhD/Workflow_Code/Phy_Bridge_Results/MI_1lPMMnoPVR_121124/"
 
 Phy_data = Phybridge_Dict(dir)
 
@@ -191,14 +191,14 @@ end
 
 display(p)
 
-
+# I should collect all STDevs by node so i can see the spread across all variables between different samples
 std_dict["Node 69"]
 mean_dict["Node 69"]
 
 savefig("Tree_Femur_STDev")
 
 plot(mars_tree, title = "Mean sampled femur trochantericfossa length", size = (1400, 800), linewidth = 2, marker_z = means, markersize = 35*std_list, linecolor = :purple)
-savefig("2lonlyFemur_TreeMeansbyvar")
+savefig("2lonlyFemur_TreeMeansbyvar2")
 plot(mars_tree, title = "Standard Deviation femur trochentericfossa length", size = (1400, 800), linewidth = 5, line_z = std_dict)
 
 savefig("Tree_femur_sdev_2lonly")
@@ -210,6 +210,7 @@ species_list
 
 std_dict
 
+using Bridge
 function Phylo_Bridge_Plot(start, final, dt, t, n)
     bridge_data = DataFrame()
     for i in 1:n
@@ -257,72 +258,6 @@ plot(x, y)
 y = collect(eachcol(test)[2])
 
 
-MI_Dir = "C:/Users/Rowan/OneDrive/Documents/GitHub/REG_PhD/Workflow_Code/MI_Data2lonly/"
-MIdict = Phybridge_Dict(MI_Dir)
-
-MIdata = MIdict["MarsMI"]
-
-MIdata.femur1
 
 
-function Phybridge_MIDict(dir)
-    try 
-        readdir(dir)
-    catch
-        println("Invalid directory")
-    end
-    Files = readdir(dir)
-
-    traitsource = CSV.read(string(dir, Files[1]), DataFrame)
-    traits = names(select(traitsource, Not([:Species, :Individual])))
-
-    df_dict = Dict{String, DataFrame}()
-# Collates all the simulated data for each trait, can do stuff to the data frames in the dictionary
-for i in traits 
-    #file_idx = findall(x -> i == split(x, "_")[1], Files)
-    trait_data = DataFrame()
-    iter = 1
-    for file in Files
-        data = CSV.read(string(dir, file), DataFrame)
-        if iter == 1
-            trait_data[!, :Species] = data[!, 1]
-        end
-        iter = iter + 1
-        trait_data[!, file] = data[!, i]
-    end
-    df_dict[i] = trait_data
-end
-return df_dict
-end 
-
-MI_dict = Phybridge_MIDict(MI_Dir)
-
-MI_femur = MI_dict["femur"]
-
-length(names(MI_femur))
-p = plot(legend = false)
-for i in 2:length(names(MI_femur))
-    vals = collect(eachcol(MI_femur)[i])
-    U = kde(vals)
-    plot!(U.x, U.density)
-end
-
-display(p)
-
-osp = filter(:Species => ==("Osphranter_robustus"), femur)
-osp = select(osp, Not([:Species]))
-osp = collect(eachrow(osp)[1])
-
-U = kde(osp)
-
-plot!(U.x, U.density, linewidth = 5, linecolor = :red)
-
-osp = filter(:Species => ==("Onychogalea_unguifera"), femur)
-osp = select(osp, Not([:Species]))
-osp = collect(eachrow(osp)[1])
-
-U = kde(osp)
-
-plot(U.x, U.density, linewidth = 5, linecolor = :blue)
-
-std(osp)
+#From here figure out what data we want to see
